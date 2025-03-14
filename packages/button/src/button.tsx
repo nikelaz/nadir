0b1af363f621235f624c454fa5757e5c03a53306
Nikola Lazarov <nikola.n.lazarov@progress.com>
2025-03-14 16:44:11 +0200

    feat: polish button states

--- packages/button/src/button.tsx
+++ packages/button/src/button.tsx
@@ -1,22 +1,38 @@
 import { useRef } from 'react';
 import type { ButtonProps } from '../types/button.types';
-import { useButton } from '@react-aria/button';
-import { useVariant, useSize } from '@nadir-ui/common';
+import { useButton, useFocusRing, useHover } from 'react-aria';
+import { mergeProps } from '@react-aria/utils';
+import { useVariant, useSize, useStateProps } from '@nadir-ui/common';
 import { clsx } from 'clsx';
 import './button.css';
+const baseClassName = 'nadir_button';
 
 export const Button = (props: ButtonProps) => {
   const ref = useRef(null);
-  const { buttonProps } = useButton(props, ref);
-  const baseClassName = 'nadir_button';
+  const { buttonProps, isPressed } = useButton(props, ref);
   const variant = useVariant(props, baseClassName);
   const size = useSize(props, baseClassName);
+  const { focusProps, isFocusVisible } = useFocusRing();
+  const { hoverProps, isHovered } = useHover({ isDisabled: props.isDisabled });
+
+  const stateProps = useStateProps([
+    { condition: isHovered, prop: 'data-hovered' },
+    { condition: isPressed, prop: 'data-pressed' },
+    { condition: isFocusVisible, prop: 'data-focus-visible' },
+  ]);
+
+  const compProps: any = mergeProps(
+    buttonProps,
+    hoverProps,
+    focusProps,
+    stateProps,
+  );
 
   return (
     <button
-      className={clsx(baseClassName, variant, size)}
       ref={ref}
-      {...buttonProps}
+      className={clsx(baseClassName, variant, size)}
+      {...compProps}
     >
       {props.children}
     </button>
