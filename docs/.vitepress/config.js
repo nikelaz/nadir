425b1e68fdcaa5bcce1e185980e64763fb861a51
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2026-03-11 21:28:05 +0200

    chore: add base path for the github pages docs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  base: '/nadir/',
   title: 'Nadir',
   description: 'A minimal, modular CSS utility library for building user interfaces.',
 
